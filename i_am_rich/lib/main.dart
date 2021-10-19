9f14eb9500cbfb9e1481c1bfbe57e458dbba6500
KaruguDev <paul.k.karugu@gmail.com>
2021-10-19 13:46:27 +0300

    i am rich demo app, flutter

---
+++ i_am_rich/lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(IAmRichApp());
+}
+
+class IAmRichApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          title: Text('I AM RICH'),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        body: Center(
+          child: Image(
+            image: AssetImage('images/diamond.png'),
+          ),
+        ),
+      ),
+    );
+  }
+}
